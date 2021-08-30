7781ddeed1a65b0ba664ff2827c5af2b12c8a532
Andre Schlaepfer <andre.aranha7@gmail.com>
2021-08-30 20:18:30 -0300

    some changes

--- project-pa/backend/api/users.route.js
+++ project-pa/backend/api/users.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {getUsers, createUser, updateUser, deleteUsers} from '../controllers/user.controller.js'
-import User from '../models/user.model.js'
+//import User from '../models/user.model.js'
 const router = express.Router()
 
 router.get('/', getUsers)
